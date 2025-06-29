5887abb762bac3b5109817d958cff1eaadaa6c20
Mark Keller <8452750+keller00@users.noreply.github.com>
2025-06-29 00:21:33 -0700

    fix: fixing original table creation database command

--- migrations/2024-08-08-053400_generate_todos/up.sql
+++ migrations/2024-08-08-053400_generate_todos/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE `todos`(
         `id` INTEGER NOT NULL PRIMARY KEY,
         `title` TEXT NOT NULL,
         `notes` TEXT NOT NULL,
-        `created` TIMESTAMPTZSQLITE NOT NULL
+        `created_on` TIMESTAMPTZSQLITE NOT NULL
 );
